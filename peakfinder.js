e3222b7ef1efb4492d91757c3bfe69a1945d876c
Dan Dzoan <ddzoan@flexport.com>
2021-04-27 13:23:39 -0700

    starting from steven's example peak finder

--- peakfinder.js
+++ peakfinder.js
@@ -1,53 +1,89 @@
+
 AFRAME.registerComponent('peakfinder', {
   init: function() {
+    console.log('init peakfinder');
     this.loaded = false;
     window.addEventListener('gps-camera-update-position', e => {
+      console.log('gps-camera-update-position', e);
       if(this.loaded === false) {
         this._loadPeaks(e.detail.position.longitude, e.detail.position.latitude);
-        // this._loadPeaks("122.4194", "37.7749");
         this.loaded = true;
       }
     });
   },
   _loadPeaks: function(longitude, latitude) {
-    console.log({longitude, latitude});
-    const scale = 2000;
-    fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${longitude-0.1},${latitude-0.1},${longitude+0.1},${latitude+0.1}&outProj=4326&format=json&poi=natural`
-    )
-      .then ( response => response.json() )
-      .then ( json => {
-        // json.features.filter ( f => f.properties.natural == 'peak' )
-        //   .forEach ( peak => {
-        //     const entity = document.createElement('a-text');
-        //     entity.setAttribute('look-at', '[gps-projected-camera]');
-        //     entity.setAttribute('value', peak.properties.name);
-        //     entity.setAttribute('scale', {
-        //       x: scale,
-        //       y: scale,
-        //       z: scale
-        //     });
-        //     entity.setAttribute('gps-projected-entity-place', {
-        //       latitude: peak.geometry.coordinates[1],
-        //       longitude: peak.geometry.coordinates[0]
-        //     });
-        //     this.el.appendChild(entity);
-        //   });
-        [["122.4394", "37.7949"]].forEach(([longitude, latitude]) => {
-          const entity = document.createElement('a-text');
-          entity.setAttribute('look-at', '[gps-projected-camera]');
-          entity.setAttribute('value', "whatever");
-          entity.setAttribute('scale', {
-            x: scale,
-            y: scale,
-            z: scale
-          });
-          entity.setAttribute('gps-projected-entity-place', {
-            latitude,
-            longitude
-          });
-          this.el.appendChild(entity);
+    const scale = 3000;
+    this._peakList.features.filter ( f => f.properties.natural == 'peak' )
+      .forEach (peak => {
+        const entity = document.createElement('a-text');
+        entity.setAttribute('look-at', '[gps-projected-camera]');
+        entity.setAttribute('value', peak.properties.name);
+        entity.setAttribute('scale', {
+          x: scale,
+          y: scale,
+          z: scale
         });
-      })
-      // });
+        entity.setAttribute('gps-projected-entity-place', {
+          latitude: peak.geometry.coordinates[1],
+          longitude: peak.geometry.coordinates[0]
+        });
+        console.log('entity is', entity, entity.getAttribute('gps-projected-entity-place'), entity.getAttribute('scale'), entity.getAttribute('look-at'));
+        this.el.appendChild(entity);
+      });
+  },
+  _peakList: {
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "osm_id": "346xxxxxx",
+          "name": "Baby Peak",
+          "natural": "peak",
+          "featuretype": "hill"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -157.79,
+            21.27
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "osm_id": "347xxxxxx",
+          "name": "Konahuanui",
+          "natural": "peak",
+          "featuretype": "hill"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -157.7887,
+            21.35320
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "osm_id": "348xxxxxx",
+          "name": "Kaala",
+          "natural": "peak",
+          "featuretype": "hill"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            // -158.142552,
+            -157.7887,
+            // 21.50788
+            21.365
+          ]
+        }
+      }
+    ],
+    "type": "FeatureCollection"
   }
 });
