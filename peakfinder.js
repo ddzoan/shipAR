7255bd1b90332a947a95be9fc4626eee18f87de0
Dan Dzoan <ddzoan@flexport.com>
2021-04-27 10:57:18 -0700

    api doesn't seem to return data, hard code some things

--- peakfinder.js
+++ peakfinder.js
@@ -3,34 +3,51 @@ AFRAME.registerComponent('peakfinder', {
     this.loaded = false;
     window.addEventListener('gps-camera-update-position', e => {
       if(this.loaded === false) {
-        // this._loadPeaks(e.detail.position.longitude, e.detail.position.latitude);
-        this._loadPeaks("122.4194", "37.7749");
+        this._loadPeaks(e.detail.position.longitude, e.detail.position.latitude);
+        // this._loadPeaks("122.4194", "37.7749");
         this.loaded = true;
       }
     });
   },
   _loadPeaks: function(longitude, latitude) {
+    console.log({longitude, latitude});
     const scale = 2000;
     fetch(`https://www.hikar.org/fm/ws/bsvr.php?bbox=${longitude-0.1},${latitude-0.1},${longitude+0.1},${latitude+0.1}&outProj=4326&format=json&poi=natural`
     )
       .then ( response => response.json() )
       .then ( json => {
-        json.features.filter ( f => f.properties.natural == 'peak' )
-          .forEach ( peak => {
-            const entity = document.createElement('a-text');
-            entity.setAttribute('look-at', '[gps-projected-camera]');
-            entity.setAttribute('value', peak.properties.name);
-            entity.setAttribute('scale', {
-              x: scale,
-              y: scale,
-              z: scale
-            });
-            entity.setAttribute('gps-projected-entity-place', {
-              latitude: peak.geometry.coordinates[1],
-              longitude: peak.geometry.coordinates[0]
-            });
-            this.el.appendChild(entity);
+        // json.features.filter ( f => f.properties.natural == 'peak' )
+        //   .forEach ( peak => {
+        //     const entity = document.createElement('a-text');
+        //     entity.setAttribute('look-at', '[gps-projected-camera]');
+        //     entity.setAttribute('value', peak.properties.name);
+        //     entity.setAttribute('scale', {
+        //       x: scale,
+        //       y: scale,
+        //       z: scale
+        //     });
+        //     entity.setAttribute('gps-projected-entity-place', {
+        //       latitude: peak.geometry.coordinates[1],
+        //       longitude: peak.geometry.coordinates[0]
+        //     });
+        //     this.el.appendChild(entity);
+        //   });
+        [["122.4394", "37.7949"]].forEach(([longitude, latitude]) => {
+          const entity = document.createElement('a-text');
+          entity.setAttribute('look-at', '[gps-projected-camera]');
+          entity.setAttribute('value', "whatever");
+          entity.setAttribute('scale', {
+            x: scale,
+            y: scale,
+            z: scale
           });
-      });
+          entity.setAttribute('gps-projected-entity-place', {
+            latitude,
+            longitude
+          });
+          this.el.appendChild(entity);
+        });
+      })
+      // });
   }
 });
