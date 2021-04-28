e203cb36ace8620045592c3e5a9e4adbee17a621
Eric McElyea <ericmcelyea@gmail.com>
2021-04-28 11:52:48 -0700

    add draw-user-coordinates debug util

---
+++ draw-user-coordinates.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('drawusercoordinates', {
+    init: function () {
+        console.log(this);
+        var reporterIds = ['lat', 'lng', 'alt'];
+        for (var x = 0; x < reporterIds.length; x++) {
+            var elem = document.createElement('div');
+            elem.id = 'report' + reporterIds[x];
+            elem.style.color = "white";
+            elem.style.position = "fixed";
+            elem.style.bottom = 8 + 14 * x + "px";
+            document.body.appendChild(elem);
+        }
+        window.addEventListener('gps-camera-update-position', e => {
+            var pos = {lat: e.detail.position.latitude.toFixed(8),
+                        lng: e.detail.position.longitude.toFixed(8),
+                        alt: e.detail.position.altitude.toFixed(8)}
+            for (var x = 0; x < reporterIds.length; x++) {
+                document.getElementById('report' + reporterIds[x]).innerText = reporterIds[x] + ": " + pos[reporterIds[x]];
+            }
+        });
+    }
+});
