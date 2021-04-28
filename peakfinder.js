72a91b64278db6017737153b1531a62f66b00b89
Dan Dzoan <ddzoan@flexport.com>
2021-04-28 10:23:21 -0700

    add some ship data

--- peakfinder.js
+++ peakfinder.js
@@ -55,6 +55,83 @@ AFRAME.registerComponent('peakfinder', {
     "features": [
       {
         "type": "Feature",
+        "properties":
+          {
+            "osm_id": 2209,
+            "name": "MATSONIA",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.303163, 37.771927]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 34717,
+            "name": "HORIZON SPIRIT",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.309125, 37.794537]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 553,
+            "name": "HYUNDAI MARS",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.320028, 37.813298]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 764,
+            "name": "EVER LOYAL",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.338888, 37.80809]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 1040,
+            "name": "MSC CAMILLE",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.314907, 37.79589]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 1780,
+            "name": "CAP SAN JUAN",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.32005, 37.797217]}
+      },
+      {
+        "type": "Feature",
+        "properties":
+          {
+            "osm_id": 745,
+            "name": "NYK CONSTELLATION",
+            "natural": "peak",
+            "featuretype": "vessel"
+          },
+        "geometry": {"type": "Point", "coordinates": [-122.322752, 37.798085]}
+      },
+      {
+        "type": "Feature",
         "properties": {
           "osm_id": "345xxxxxx",
           "name": "Twin\nPeaks",
