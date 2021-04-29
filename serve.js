756db3a1f708054d2bc9b2ada79932b29d729386
CY Tan <cytan93@gmail.com>
2021-04-28 18:06:14 -0700

    fix url

--- serve.js
+++ serve.js
@@ -1,48 +1,45 @@
-const localtunnel = require('localtunnel');
-const http = require('http');
-const qrcode = require('qrcode-terminal');
+const localtunnel = require("localtunnel");
+const http = require("http");
+const qrcode = require("qrcode-terminal");
 
-const mime = require('mime-types');
+const mime = require("mime-types");
 
-const fs = require('fs');
-
+const fs = require("fs");
 
 const port = 8080;
 
+http
+  .createServer(function (req, res) {
+    const file = req.url.substring(1);
 
-http.createServer(function (req, res) {  
-  const file = req.url.substring(1);
-
-  try {
-    if (!fs.existsSync(file)) {
-      res.writeHead(404, { 'content-type': 'text/plain' });
-      res.write(`File not found: ${file}`);
+    try {
+      if (!fs.existsSync(file)) {
+        res.writeHead(404, { "content-type": "text/plain" });
+        res.write(`File not found: ${file}`);
+        res.end();
+        return;
+      }
+      const fsStream = fs.createReadStream(file);
+      res.writeHead(200, { "content-type": mime.lookup(file) });
+      fsStream.pipe(res);
+    } catch (e) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      res.write(e.toString());
       res.end();
-      return;
     }
-    const fsStream = fs.createReadStream(file);
-    res.writeHead(200, { 'content-type': mime.lookup(file) });
-    fsStream.pipe(res);
-  }
-  catch(e) {
-    res.writeHead(500, { 'content-type': 'text/plain' });
-    res.write(e.toString());
-    res.end();
-  }
-}).listen(port);
-
+  })
+  .listen(port);
 
 (async () => {
   const tunnel = await localtunnel({ port });
 
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
-  const url = tunnel.url + "/step1-location.html";
-  console.log('load app on your mobile device at', url);
+  const url = tunnel.url + "/index.html";
+  console.log("load app on your mobile device at", url);
   qrcode.generate(url);
 
-
-  tunnel.on('close', () => {
+  tunnel.on("close", () => {
     // tunnels are closed
   });
 })();
